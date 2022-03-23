7d010dac8c5437dd5c9a03af75012512a0697338
Andres Otarola <andres@otarola.me>
2022-03-22 20:46:54 -0700

    better config

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import './style.css';
 import { Elm } from './src/Main.elm';
 
 const root = document.querySelector('#app div');
+
 Elm.Main.init({ node: root });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import elmPlugin from "vite-plugin-elm";
+import { defineConfig } from 'vite';
+
+import elmPlugin from 'vite-plugin-elm';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [elmPlugin(), eslintPlugin()],
 });
